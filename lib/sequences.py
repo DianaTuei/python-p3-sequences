66bd0dcc1daf86df60044a3d70d075bcbb326882
DianaTuei <memotuei@gmail.com>
2024-03-03 06:53:57 +0300

    Completed Assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    empty_li = []  # Initialize an empty list to store the Fibonacci sequence
+    for i in range(length):  # Loop from 0 to length-1
+        if i < 2:
+            empty_li.append(i)  # If i is 0 or 1, add i to the sequence
+        else:
+            empty_li.append(empty_li[-1] + empty_li[-2])  # Add the last two numbers in the sequence to get the next one
+
+    print(empty_li)  # Print the resulting Fibonacci sequence
+
+print_fibonacci(9)  # Call the function with a length of 9
